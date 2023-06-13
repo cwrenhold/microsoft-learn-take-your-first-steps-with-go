e85f2e5d923d6eeb9ce733f78f6da8e59e24e1ec
Catherine Wrenhold <40322939+cwrenhold@users.noreply.github.com>
2023-06-13 19:43:52 +0100

    Added two challenges from module-2.

---
+++ module-2/challenge-1/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var someValue int = 999999999999999999999
+	fmt.Println(someValue)
+}
---
+++ module-2/challenge-2/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var someValue uint = -10
+	fmt.Println(someValue)
+}
